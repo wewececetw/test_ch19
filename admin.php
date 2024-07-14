69e76e746c2195b8f8405a289e520884832e24c3
Kento <rogeraabbccdd@gmail.com>
2024-07-14 16:07:05 +0800

    feat: 圖片驗證

--- admin.php
+++ admin.php
@@ -1,3 +1,9 @@
+<style>
+    #圖層_1 {
+        width: 200px;
+        height: 100px;
+    }
+</style>
 
 <section id="login" class="py-5">
     <div class="container border border-secondary p-4">
@@ -16,16 +22,63 @@
                 <div class="col-9 mb-2">
                     <input type="text" class="form-control" id="img_pw">
                 </div>
-                <div class="col-3 col-form-label"></div>
-                <div class="col-9 text-right">
-                    <span class="mx-2">1234</span>
-                    <button class="btn btn-secondary">驗證碼重新產生</button>
+                <div class="col-12 col-form-label text-center">
+                    <svg version="1.1" id="圖層_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 200 100"
+                        style="enable-background:new 0 0 200 100;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0 {
+                                font-family: 'AdobeMingStd-Light-B5pc-H';
+                            }
+
+                            .st1 {
+                                font-size: 48px;
+                            }
+                        </style>
+                        <text transform="matrix(1 0 0 1 10.7417 47.2579)" class="st0 st1">A</text>
+                        <text transform="matrix(1 0 0 1 56.8066 61.5811)" class="st0 st1">B</text>
+                        <text transform="matrix(1 0 0 1 101.129 57.7095)" class="st0 st1">C</text>
+                        <text transform="matrix(1 0 0 1 154.7421 88.6777)" class="st0 st1">D</text>
+                    </svg>
                 </div>
                 <div class="col-12 text-center">
-                    <button class="btn btn-secondary mx-2">送出</button>
+                    <div class="form-inline justify-content-center">
+                        <input type="text" class="form-control mr-2" name="" id="code">
+                        <button id="ref-code" class="btn btn-secondary">驗證碼重新產生</button>
+                    </div>
+                </div>
+                <div class="col-12 text-center mt-2">
+                    <button class="btn btn-secondary mx-2 code-check">送出</button>
                     <button class="btn btn-secondary">重設</button>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<script>
+    let code = ''
+
+    $('#圖層_1 text').each(function (index) {
+        const x = index * 50 + Math.round(Math.random() * 30)
+        const y = 25 + Math.round(Math.random() * 50)
+        $(this).attr('transform', `matrix(1 0 0 1 ${x} ${y})`)
+        const text = String.fromCharCode(65 + Math.round(Math.random() * (90 - 65)))
+        $(this).text(text)
+        code += text
+    })
+    $('#ref-code').on('click', function () {
+        $('#圖層_1 text').each(function (index) {
+            const x = index * 50 + Math.round(Math.random() * 30)
+            const y = 25 + Math.round(Math.random() * 50)
+            $(this).attr('transform', `matrix(1 0 0 1 ${x} ${y})`)
+            const text = String.fromCharCode(65 + Math.round(Math.random() * (90 - 65)))
+            $(this).text(text)
+            code += text
+        })
+    })
+
+    $('.code-check').click(function () {
+        alert(code == $('#code').val().toUpperCase() ? 'OK' : 'X')
+    })
+</script>
